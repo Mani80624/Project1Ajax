5bbb046c82819ce9b59741b60e506c0c8e6baf11
mani80624 <manueldolores01@gmail.com>
2024-09-21 06:42:59 -0600

    Create Repository AJAX

---
+++ js/ajax.js
@@ -0,0 +1,6 @@
+console.log('Hola desde consola');
+
+document.querySelector("#button_ajax").addEventListener("click",dataGet);
+function dataGet(){
+    console.log("Hola desde función dataGet");
+}

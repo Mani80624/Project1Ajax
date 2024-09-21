d1fcee7610dc113d938f712e361793cb63aca984
mani80624 <manueldolores01@gmail.com>
2024-09-21 08:40:24 -0600

    Async function

--- js/ajax.js
+++ js/ajax.js
@@ -3,6 +3,14 @@ console.log('Hola desde consola');
 document.querySelector("#button_ajax").addEventListener("click",dataGet);
 function dataGet(){
     const http = new XMLHttpRequest();
-    http.open("GET","/txt/archivo.txt");
+    // True porque es asincronico 
+    http.open("GET","/txt/archivo.txt", true);
     http.send();
+    http.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            // Verificar en consola
+            //console.log(this.responseText);
+            document.querySelector("#answer").innerHTML = this.responseText;
+        }
+    }
 }
